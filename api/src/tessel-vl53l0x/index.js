823ce060d185485fa591df9514e27492c5548619
NITESCU Cristian <az02289@FRLA000112.local>
2018-07-11 18:17:52 +0200

    Error management

--- api/src/tessel-vl53l0x/index.js
+++ api/src/tessel-vl53l0x/index.js
@@ -385,6 +385,7 @@ class VL53LOX extends EventEmitter {
 		this._writeRegisters(REGISTRY.SYSRANGE_START, 0x02);
 
     this._readRegisters(REGISTRY.RESULT_RANGE_STATUS, 16, (err, data) => {
+		if (err) console.log('ERROR', err);
       var _dis = (data.readInt16BE(8) + 10);
       this.emit('distance', _dis);
     });
