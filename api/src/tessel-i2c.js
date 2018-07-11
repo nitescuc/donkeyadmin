da4adbd1846bcbeb15329f8557e76a887fe1434d
NITESCU Cristian <az02289@FRLA000112.local>
2018-07-11 16:28:24 +0200

    change laser driver

---
+++ api/src/tessel-i2c.js
@@ -0,0 +1,18 @@
+const i2c = require('i2c-bus');
+
+class I2C {
+    constructor (address) {
+        this.i2c = i2c.openSync(1);
+        this.address = address;
+    }
+    transfer(addressToRead, bytesToRead, cb) {
+        this.i2c.readI2cBlock(this.address, addressToRead[0], bytesToRead, new Buffer(bytesToRead), (err, length, buffer) => {
+            cb(err, buffer);
+        });
+    }
+    send(buffer, cb) {
+        this.i2c.writeByte(this.address, buffer[0], buffer[1], cb);
+    }
+}
+
+module.exports = I2C;
--- api/test/test_laser.js
+++ api/test/test_laser.js
@@ -1,7 +1,11 @@
-const VL53L0X = require('../src/vl53l0x/vl53l0x');
+const VL53L0X = require('../src/tessel-vl53l0x');
+const I2C = require('../src/tessel-i2c');
 
-const sensor = new VL53L0X();
+const sensor = new VL53L0X({
+    IC2
+});
 
-while (true) {
-    sensor.readRangeSingleMillimiters();
-}
+sensor.on('distance', (message) => {
+    console.log(message);
+})
+sensor.startCapture();
