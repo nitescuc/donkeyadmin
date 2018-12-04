82491e3f3da1f93adddf1b682b2c6632c9679884
NITESCU Cristian <cristian.nitescu@renault.com>
2018-12-04 21:16:53 +0100

    API change

--- app/src/api/tubes.api.js
+++ app/src/api/tubes.api.js
@@ -1,7 +1,7 @@
 const request = require('superagent');
 
 export const getTubes = async (baseUrl, onMessage) => {
-    const url = `${baseUrl || ''}/tubes`;
+    const url = `${baseUrl || ''}/api/tubes`;
     const resp = await request.get(url);
     if (onMessage) onMessage(`${new Date().toISOString()} - GET - ${url} - ${resp.status}`);
     return (resp || {}).body || [];
